868e5877c46a79313f7d37ce261132d16b4c0b6f
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-02-20 16:34:46 +0100

    Cors header

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "https://ultimate-tic-tac-toe-xi.vercel.app/",
+    origin: "https://*.vercel.app/",
     methods: ["GET", "POST"],
   },
 });
