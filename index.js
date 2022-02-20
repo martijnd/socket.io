0c57acfe1425d274633fb8ca47b372928b36dac6
Martijn Dorsman <martijn.dorsman@gmail.com>
2022-02-20 16:17:20 +0100

    set CORS

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
